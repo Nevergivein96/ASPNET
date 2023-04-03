1aad93f878533f4183d67fb68a2b41ea67e644d0
Spencer Hansen <spencerhansen@gmail.com>
2023-04-03 16:11:03 -0600

    finished the update portion

--- Testing/ProductRepository.cs
+++ Testing/ProductRepository.cs
@@ -21,5 +21,10 @@ namespace Testing
         {
             return _conn.QuerySingle<Product>("select * from products where productid = @id", new {id = id});
         }
+        public void UpdateProduct(Product product)
+        {
+            _conn.Execute("UPDATE products SET Name = @name, Price = @price WHERE ProductID = @id",
+             new { name = product.Name, price = product.Price, id = product.ProductID });
+        }
     }
 }
